6d7c1ff1ddd9cb7b373db9f860c82f461839e2ef
Вова <v.grishchenko@onpoint.ru>
2026-09-20 21:03:32 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,4 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ["react", "react-dom", "react-router-dom"],
-          supabase: ["@supabase/supabase-js"],
-          ui: ["lucide-react", "react-hot-toast", "canvas-confetti"],
-        },
-      },
-    },
-  },
 });
